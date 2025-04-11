255d5d0e9009655f13eb7a0bfed7167eccf34bcd
jambrocio <jc.ambrocio.sernaque@gmail.com>
2025-04-10 20:47:00 -0500

    Initial commit

---
+++ src/main/java/com/example/api/api_first_example/ApiFirstExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.api.api_first_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiFirstExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiFirstExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/api/api_first_example/api_first_example/ApiFirstExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.api.api_first_example.api_first_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiFirstExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
